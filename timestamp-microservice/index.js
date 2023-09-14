92aa840db635d1da02daf10b7c902c0445113d54
Bhupinder Singh <bs082045@gmail.com>
2023-09-14 17:14:23 -0400

    complete timestamp microservice project

--- timestamp-microservice/index.js
+++ timestamp-microservice/index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,11 +19,52 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// date api endpoint 
+app.get("/api/:date?", function (req, res) {
 
+
+  const date = req.params.date
+
+  // empty date
+  if (!date) {
+
+    return res.json({
+      unix: Date.now(),
+      utc: new Date().toUTCString()
+    })
+
+  }
+
+
+  // timestamp
+  if (!isNaN(date)) {
+
+    return res.json({
+      unix: new Date(parseInt(date)).getTime(),
+      utc: new Date(parseInt(date)).toUTCString()
+    })
+
+  }
+
+  // valid date
+  if (isNaN(date) && new Date(date) != 'Invalid Date') {
+
+    return res.json({
+      unix: new Date(date).getTime(),
+      utc: new Date(date).toUTCString()
+    })
+
+  }
+
+  // invalid date
+  if (new Date(date) == 'Invalid Date') {
+
+    return res.json({ error: 'Invalid Date' })
+
+  }
+
+
+});
 
 
 // listen for requests :)
