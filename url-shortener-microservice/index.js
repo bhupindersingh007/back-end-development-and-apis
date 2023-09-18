977feeca24d750f3d674bbd043348f80111e346d
Bhupinder Singh <bs082045@gmail.com>
2023-09-18 00:26:24 -0400

    implement POST api route and use express body parser

--- url-shortener-microservice/index.js
+++ url-shortener-microservice/index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser= require('body-parser')
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,8 +18,12 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+
+  const url = req.body.url 
+
+  res.json({ url: url });
+
 });
 
 app.listen(port, function() {
