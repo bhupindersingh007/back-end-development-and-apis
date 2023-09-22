7f34a9b77884839ad0dc69d0d4dd34b57d539a0d
Bhupinder Singh <bs082045@gmail.com>
2023-09-22 14:23:33 -0400

    create .env file, install mongoose (monogo db ORM) and connect to DB

--- exercise-tracker/index.js
+++ exercise-tracker/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+dotenv.config()
 
 app.use(cors())
 app.use(express.static('public'))
 
 app.use(express.urlencoded({ extended : true }))
+
+mongoose.connect(process.env.MONGO_URI)
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
