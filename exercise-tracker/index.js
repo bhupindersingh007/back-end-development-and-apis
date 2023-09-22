2ebfd5d1ce0fe1c884e73dac0405354a57bedc7b
Bhupinder Singh <bs082045@gmail.com>
2023-09-22 14:33:26 -0400

    make User model and implement POST /api/users API

--- exercise-tracker/index.js
+++ exercise-tracker/index.js
@@ -3,13 +3,14 @@ const app = express()
 const cors = require('cors')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const User = require('./models/User')
 
 dotenv.config()
 
 app.use(cors())
 app.use(express.static('public'))
 
-app.use(express.urlencoded({ extended : true }))
+app.use(express.urlencoded({ extended: true }))
 
 mongoose.connect(process.env.MONGO_URI)
 
@@ -30,11 +31,13 @@ app.get('/api/users', (req, res) => {
 })
 
 // store user
-app.post('/api/users', (req, res) => {
+app.post('/api/users', async (req, res) => {
+
+  const user = await User.create({ username: req.body.username })
 
   res.json({
-    _id: '5fb5853f734231456ccb3b05',
-    username: 'someone'
+    username: user.username,
+    _id: user._id
   });
 
 })
@@ -62,9 +65,9 @@ app.get('/api/users/:_id/logs', (req, res) => {
     count: 1,
     _id: '5fb5853f734231456ccb3b05',
     log: [
-      { description: 'something', duration: 60, date: 'Mon Jan 01 1990'},
-      { description: 'something', duration: 60, date: 'Mon Jan 02 1990'},
-      { description: 'something', duration: 60, date: 'Mon Jan 03 1990'},
+      { description: 'something', duration: 60, date: 'Mon Jan 01 1990' },
+      { description: 'something', duration: 60, date: 'Mon Jan 02 1990' },
+      { description: 'something', duration: 60, date: 'Mon Jan 03 1990' },
     ]
   })
 
